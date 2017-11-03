8760b8fa09996f4998e3ae660098e773c97a1d80
kjwong2018 <kjwong@buffalo.edu>
2017-11-03 12:31:51 -0400

    Added Datapack and 2 weapon class.

---
+++ Assets/Combat/Model/RainingArrows.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainingArrows:FirstCannon{
+
+	public RainingArrows(){
+		WeaponName = "Raining Arrows";
+		WeaponCooldown = 3f;
+		CurrentCooldown = 0f;
+		WeaponAttack = 20;
+	}
+}
---
+++ Assets/Combat/Model/Winchester.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Winchester : FirstCannon{
+	
+	public Winchester(){
+		WeaponName = "Winchester";
+		WeaponCooldown = 3f;
+		CurrentCooldown = 0f;
+		WeaponAttack = 30;
+	}
+}
