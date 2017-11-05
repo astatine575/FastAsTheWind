3cad20a3c1f25f04fb635e21ca890c3874c40b8e
Henry <henryohm@buffalo.edu>
2017-11-05 13:53:15 -0500

    Addition of RockHazard and WhirlpoolRadius files for collaboration. Issues with getting the _hullhealth from Ship script, not sure if that's the variable I should be editing to decrease the health of the player's ship.

---
+++ Assets/WorldMap/Scripts/RockHazard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockHazard : MonoBehaviour {
+	public bool collided;
+
+	// Use this for initialization
+	void Start () {
+		collided = false;
+		ship = GetComponent<Ship> ();
+	}
+
+	public void OnTriggerEnter2D (Collider2D collision) {
+		collided = true;
+	}
+
+	public void OnTriggerExit2D (Collider2D collision) {
+		collided = false;
+	}
+	
+	// Update is called once per frame
+	public void FixedUpdate () {
+		while (collided) {
+			ship._hullhealth = ship._hullhealth - 5;
+		}
+		// if the player collided with this object raduis, remove a certain amount of
+		// health each update until they exit the radius
+	}
+}
